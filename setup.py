66dd965b46b417eff8f543c5ed083f0b17396008
Waqar Ahmed <waqarahmedjoyia@live.com>
2025-08-30 00:57:57 +0500

    Fix build for debian trixie

--- setup.py
+++ setup.py
@@ -5,7 +5,8 @@ Exts = [
     Extension("libsg3.ses", ["libsg3/ses/ses.pyx"],
               libraries=["sgutils2"],
               library_dirs=["/usr/lib"],
-              include_dirs=["/usr/include"])
+              include_dirs=["/usr/include"],
+              define_macros=[("HAVE_LINUX_TYPES_H", None)])
 ]
 
 setup(
